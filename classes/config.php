61eff9588ac9b37a69c493fd565b1a96cccc4daa
npetrov <npetrov2309@gmail.com>
2022-02-28 00:26:39 +0500

    Создание логики конфига

--- classes/config.php
+++ classes/config.php
@@ -1,40 +1,21 @@
 <?
 class Config {
-    private $config_cache = "";
-    private $config_path =  DIRECTORY_SEPARATOR . "configs" . DIRECTORY_SEPARATOR;
+    private static $config_cache = "";
 
     public static function get_config(string $config_name, string $config_key = "") {
-        return $this->include_config($config_name, $config_key);
+        return self::include_config($config_name, $config_key);
     }
 
-    public function include_config(string $config_name, string $config_key = "") {
-        if($this->config_cache == "") {
-            if(file_exists($this->config_path . $config_name . ".php")) {
-                $this->config_cache = include_once($this->config_path . $config_name . ".php");
-                return array_key_exists($config_key, $this->config_cache) ? $this->config_cache[$config_key] : $this->config_cache;
+    public static function include_config(string $config_name, string $config_key = "") {
+        $path = $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "configs" . DIRECTORY_SEPARATOR . $config_name . ".php";
+        if(self::$config_cache == "") {
+            if(file_exists($path)) {
+                self::$config_cache = include_once($path);
+                return array_key_exists($config_key, self::$config_cache) ? self::$config_cache[$config_key] : self::$config_cache;
             }
         }
         else {
-            return $this->config_cache;
+            return self::$config_cache;
         }
     }
-    // public static function get_config(string $config_name, string $config_key = "") {
-    //     $path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "configs" . DIRECTORY_SEPARATOR . $config_name . ".php";
-    //     if (self::$config_cache == "") {
-    //         if (file_exists($path)) {
-    //             self::$config_cache = include_once($path);
-    //             if ($config_key == "") {
-    //                 return self::$config_cache;
-    //             }
-    //             else
-    //             {
-    //                 return array_key_exists($config_key, self::$config_cache) ? self::$config_cache[$config_key] : null;
-    //             }
-    //         }
-    //     }
-    //     else
-    //     {
-    //         return self::$config_cache[$config_key];
-    //     }
-    // }
 }
